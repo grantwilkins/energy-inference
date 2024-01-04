250c3f4b5ecd4c82130135b528dbc72bb43d7a67
Grant Wilkins <ac.gwilkins@gpulogin1.lcrc.anl.gov>
2024-01-04 08:34:41 -0600

    mistral

---
+++ mistral/mistral7b.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+model_name = "mistralai/Mistral-7B-v0.1"
+model = AutoModelForCausalLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+inputs = tokenizer("The sky is blue because", return_tensors="pt")
+model.to("cuda")
+inputs = inputs.to("cuda")
+with torch.no_grad():  # Disable gradient calculations for inference
+    outputs = model(**inputs)
+token_ids = outputs[0].tolist()
+print(outputs)
+# Decode the list of token ids to text
+predicted_text = tokenizer.decode(token_ids, skip_special_tokens=True)
+
+
