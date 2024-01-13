5b0fe568e31f90cd1e38ee4e907a45b00b4806dd
Grant Wilkins <ac.gwilkins@gpulogin1.lcrc.anl.gov>
2024-01-13 09:49:54 -0600

    mistral update

--- mistral/mistral7b.py
+++ mistral/mistral7b.py
@@ -1,20 +1,36 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import torch
 from pyJoules.energy_meter import EnergyContext
 from pyJoules.device.nvidia_device import NvidiaGPUDomain
+from pyJoules.handler.csv_handler import CSVHandler
+
 def run_inference():
 	model_name = "mistralai/Mistral-7B-v0.1"
-	model = AutoModelForCausalLM.from_pretrained(model_name)
 	tokenizer = AutoTokenizer.from_pretrained(model_name)
-	inputs = tokenizer("The sky is blue because", return_tensors="pt")
-	model.to("cuda")
-	inputs = inputs.to("cuda")
-	with torch.no_grad():  # Disable gradient calculations for inference
-		outputs = model(**inputs)
-	token_ids = outputs[0].tolist()
-	print(outputs)
-	# Decode the list of token ids to text
-	predicted_text = tokenizer.decode(token_ids, skip_special_tokens=True)
+	model = AutoModelForCausalLM.from_pretrained(
+        	model_name,
+        	torch_dtype=torch.bfloat16,
+        	device_map="auto",
+        	trust_remote_code=True)	
+	pipe = pipeline(
+    		"text-generation", 
+    		model=model, 
+    		tokenizer = tokenizer, 
+    		torch_dtype=torch.bfloat16, 
+    		device_map="auto")
+	prompt = "As a data scientist, can you explain the concept of regularization in machine learning?"
+	sequences = pipe(
+    		prompt,
+    		do_sample=True,
+    		max_new_tokens=100, 
+    		temperature=0.7, 
+    		top_k=50, 
+    		top_p=0.95,
+    		num_return_sequences=1,)
+	print(sequences[0]['generated_text'])
 
-with EnergyContext(domains=[NvidiaGPUDomain(0)]):
+csv_handle = CSVHandler("results.csv")
+with EnergyContext(handler=csv_handle, domains=[NvidiaGPUDomain([0,1])]):
 	run_inference()
+
+csv_handle.save_data()
