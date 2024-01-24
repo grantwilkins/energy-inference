ca434b0fb16f3484e6f7dea30ad7f8186f785c4a
Grant Wilkins <ac.gwilkins@gpulogin2.lcrc.anl.gov>
2024-01-24 05:55:59 -0600

    add larger models

---
+++ mistral/mistral8x7b-cuda.py
@@ -0,0 +1,47 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import torch
+from pyJoules.energy_meter import EnergyContext
+from pyJoules.device.nvidia_device import NvidiaGPUDomain
+from pyJoules.handler.csv_handler import CSVHandler
+
+num_gpus = torch.cuda.device_count()
+csv_handle = CSVHandler(f"mixtral8x7b-cuda-{num_gpus}.csv")
+num_tokens = 200
+#domains = NvididaGPUDomain0 if num_gpus == 1 else [i for i in range(num_gpus)]
+
+with EnergyContext(
+    handler=csv_handle,
+    domains=[NvidiaGPUDomain(i) for i in range(num_gpus)],
+    start_tag="tokenizer",
+) as ctx:
+    model_name = "mistralai/Mixtral-8x7B-v0.1"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    ctx.record(tag="model load")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        trust_remote_code=True,
+    )
+    ctx.record(tag="pipeline load")
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+    )
+    ctx.record(tag="inference")
+    prompt = "As a data scientist, can you explain the concept of regularization in machine learning?"
+    sequences = pipe(
+        prompt,
+        do_sample=True,
+        max_new_tokens=num_tokens,
+        temperature=0.7,
+        top_k=50,
+        top_p=0.95,
+        num_return_sequences=1,
+    )
+    print(sequences[0]["generated_text"])
+
+csv_handle.save_data()
