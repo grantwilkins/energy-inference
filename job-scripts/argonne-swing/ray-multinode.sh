3ad2250b5ad905edd7681ad4fadf9f3ad03b6196
Grant <grant.wilkins371@gmail.com>
2024-02-20 11:07:04 +0000

    ray stuff

--- job-scripts/argonne-swing/ray-multinode.sh
+++ job-scripts/argonne-swing/ray-multinode.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
-#SBATCH --job-name=alpa_multinode_test
+
+#SBATCH -J ray-multinode
+#SBATCH --time=00:10:00
 #SBATCH --nodes=2
+### Give all resources on each node to a single Ray task, ray can manage the resources internally
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=1:00:00 
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=32
 
-# Get names of nodes assigned
-nodes=$(scontrol show hostnames "$SLURM_JOB_NODELIST")
-nodes_array=($nodes)
 
-# By default, set the first node to be head_node on which we run HEAD of Ray
-head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+# Load modules or your own conda environment here
+module load cuda/11.1.1-nkh7mm7 cudnn/8.0.5.39
+conda activate alpa-inference
 
-# Setup port and variables needed
-gpus_per_node=1
-port=20000
-ip_head=$head_node_ip:$port
-export ip_head
-# Start HEAD in background of head node
-srun --nodes=1 --ntasks=1 -w "$head_node" \
-        ray start --head --node-ip-address="$head_node_ip" --port=$port \
-        --num-cpus "${SLURM_CPUS_PER_TASK}" --num-gpus $gpus_per_node --block &
+head_node=$(hostname)
+head_node_ip=$(hostname --ip-address)
+# if we detect a space character in the head node IP, we'll
+# convert it to an ipv4 address. This step is optional.
+if [[ "$head_node_ip" == *" "* ]]; then
+IFS=' ' read -ra ADDR <<<"$head_node_ip"
+if [[ ${#ADDR[0]} -gt 16 ]]; then
+  head_node_ip=${ADDR[1]}
+else
+  head_node_ip=${ADDR[0]}
+fi
+fi
+port=6379
 
-# Optional, sometimes needed for old Ray versions
+echo "STARTING HEAD at $head_node"
+echo "Head node IP: $head_node_ip"
+srun --nodes=1 --ntasks=1 -w $head_node start-head.sh $head_node_ip &
 sleep 10
 
-# Start worker nodes
-# Number of nodes other than the head node
-worker_num=$((SLURM_JOB_NUM_NODES - 1))
-# Iterate on each node other than head node, start ray worker and connect to HEAD
-for ((i = 1; i <= worker_num; i++)); do
-    node_i=${nodes_array[$i]}
-    echo "Starting WORKER $i at $node_i"
-    srun --nodes=1 --ntasks=1 -w "$node_i" \
-        ray start --address "$ip_head" --num-cpus "${SLURM_CPUS_PER_TASK}" \
-        --num-gpus $gpus_per_node --block &
-    sleep 5
-done
+worker_num=$(($SLURM_JOB_NUM_NODES - 1)) #number of nodes other than the head node
+srun -n $worker_num --nodes=$worker_num --ntasks-per-node=1 --exclude $head_node start-worker.sh $head_node_ip:$port &
+sleep 5
+##############################################################################################
 
-# Run Alpa test
+#### call your code below
 python3 ../../cuda/open-source-cuda-ray.py
 
-# Optional. Slurm will terminate all processes automatically
-ray stop
-conda deactivate
 exit
