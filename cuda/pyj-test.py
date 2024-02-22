370bf3b0bb5b179483642684f049df95659dc275
Grant <grant.wilkins371@gmail.com>
2024-02-22 15:31:53 +0000

    nodeid

--- cuda/pyj-test.py
+++ cuda/pyj-test.py
@@ -4,8 +4,11 @@ from pyJoules.device.nvidia_device import NvidiaGPUDomain
 from pyJoules.handler.csv_handler import CSVHandler
 import argparse
 from time import sleep
+import os
 
 parser = argparse.ArgumentParser()
+
+node_id = os.environ.get("SLURM_NODEID")
 
 parser.add_argument("--num_tokens", type=int, default=200)
 parser.add_argument("--model_name", type=str, default="pyjoules")
@@ -15,7 +18,7 @@ args = parser.parse_args()
 
 num_gpus = torch.cuda.device_count()
 stats_name = args.model_name
-csv_handle = CSVHandler(f"{stats_name}-{num_gpus}.csv")
+csv_handle = CSVHandler(f"{stats_name}-{node_id}-{num_gpus}.csv")
 num_tokens = args.num_tokens
 # domains = NvididaGPUDomain0 if num_gpus == 1 else [i for i in range(num_gpus)]
 
