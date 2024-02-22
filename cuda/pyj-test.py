a032f559fb553eaec4c44b7f9d4aafff1f5f3de0
Grant Wilkins <ac.gwilkins@gpu1.lcrc.anl.gov>
2024-02-22 09:30:28 -0600

    update pyj

--- cuda/pyj-test.py
+++ cuda/pyj-test.py
@@ -8,13 +8,13 @@ from time import sleep
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--num_tokens", type=int, default=200)
-parser.add_argument("--model_name", type=str, default="mistralai/Mistral-7B-v0.1")
+parser.add_argument("--model_name", type=str, default="pyjoules")
 
 args = parser.parse_args()
 
 
 num_gpus = torch.cuda.device_count()
-stats_name = args.model_name.split("/")[1]
+stats_name = args.model_name
 csv_handle = CSVHandler(f"{stats_name}-{num_gpus}.csv")
 num_tokens = args.num_tokens
 # domains = NvididaGPUDomain0 if num_gpus == 1 else [i for i in range(num_gpus)]
