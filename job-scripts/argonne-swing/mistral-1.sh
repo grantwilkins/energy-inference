17fbbf0b25dc07440836a12c0826e63bd3e3209f
Grant <grant.wilkins371@gmail.com>
2024-03-17 20:37:46 +0000

    script fix

--- job-scripts/argonne-swing/mistral-1.sh
+++ job-scripts/argonne-swing/mistral-1.sh
@@ -8,14 +8,14 @@
 #SBATCH --gres=gpu:1
 
 cd /home/ac.gwilkins/energy-inference/cuda/
-python3 open-source-cuda.py --num_tokens 128 --model_name mistralai/Mistral-7B-v0.1 
-python3 open-source-cuda.py --num_tokens 256 --model_name mistralai/Mistral-7B-v0.1 
-python3 open-source-cuda.py --num_tokens 512 --model_name mistralai/Mistral-7B-v0.1 
-python3 open-source-cuda.py --num_tokens 1024 --model_name mistralai/Mistral-7B-v0.1
-python3 open-source-cuda.py --num_tokens 2048 --model_name mistralai/Mistral-7B-v0.1
+python3 open-source-cuda.py --num_tokens 128 --hf_name mistralai/Mistral-7B-v0.1 
+python3 open-source-cuda.py --num_tokens 256 --hf_name mistralai/Mistral-7B-v0.1 
+python3 open-source-cuda.py --num_tokens 512 --hf_name mistralai/Mistral-7B-v0.1 
+python3 open-source-cuda.py --num_tokens 1024 --hf_name mistralai/Mistral-7B-v0.1
+python3 open-source-cuda.py --num_tokens 2048 --hf_name mistralai/Mistral-7B-v0.1
 
-python3 open-source-cuda.py --num_tokens 512 --model_name mistralai/Mistral-7B-v0.1 --batch_size 8
-python3 open-source-cuda.py --num_tokens 512 --model_name mistralai/Mistral-7B-v0.1 --batch_size 16
-python3 open-source-cuda.py --num_tokens 512 --model_name mistralai/Mistral-7B-v0.1 --batch_size 32
-python3 open-source-cuda.py --num_tokens 512 --model_name mistralai/Mistral-7B-v0.1 --batch_size 64
-python3 open-source-cuda.py --num_tokens 512 --model_name mistralai/Mistral-7B-v0.1 --batch_size 128
+python3 open-source-cuda.py --num_tokens 512 --hf_name mistralai/Mistral-7B-v0.1 --batch_size 8
+python3 open-source-cuda.py --num_tokens 512 --hf_name mistralai/Mistral-7B-v0.1 --batch_size 16
+python3 open-source-cuda.py --num_tokens 512 --hf_name mistralai/Mistral-7B-v0.1 --batch_size 32
+python3 open-source-cuda.py --num_tokens 512 --hf_name mistralai/Mistral-7B-v0.1 --batch_size 64
+python3 open-source-cuda.py --num_tokens 512 --hf_name mistralai/Mistral-7B-v0.1 --batch_size 128
