88fc215e0b3453f0763341ef0162c39ff328a38c
Grant <grant.wilkins371@gmail.com>
2024-03-27 09:49:22 +0000

    whole lotta changes

---
+++ job-scripts/argonne-swing/llama2-7b-2-input-tokens.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -J llama2-7b-2-input-tokens
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --time=6:00:00 
+#SBATCH --gres=gpu:2
+
+N_NODES=1
+N_GPUS_PER_NODE=2
+N_GPUS=$((N_NODES * N_GPUS_PER_NODE))
+N_TOKENS=(128 256 512 1024 2048 4096)
+HF_NAME="meta-llama/Llama-2-7b-chat-hf"
+MODEL_NAME="llama2-7b"
+SYSTEM="argonne-swing"
+DATE=$(date +"%Y-%m-%d")
+TIME=$(date +"%H-%M-%S")
+module load amd-uprof
+cd /home/ac.gwilkins/energy-inference/cuda/
+
+for num_tokens in "${N_TOKENS[@]}"
+	do 
+        AMDuProfCLI timechart --event power --interval 100 --duration 99999 -o ./$MODEL_NAME/$DATE/$TIME/ python3 gated-cuda.py --out_dir ./$MODEL_NAME/$DATE/$TIME --num_tokens $num_tokens --hf_name $HF_NAME --system_name $SYSTEM
+    done
